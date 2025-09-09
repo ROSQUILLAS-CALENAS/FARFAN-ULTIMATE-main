86ea453d46df0e63cd57c482b03d7ffcf3cbd497
THEBLESSMAN687 <juan.rave@utp.edu.co>
2025-09-09 12:11:17 -0500

    S
    H

--- retrieval_trace.py
+++
@@ -1,3 +0,0 @@
-"""Retrieval trace module stub"""
-def trace_retrieval(query):
-    return {"query": query, "results": []}
--- traceability.py
+++
@@ -1,3 +0,0 @@
-"""Traceability module stub"""
-def trace_execution(func):
-    return func
